84461e51a22323619c21cac9ae54aa929c8028f8
AlirezaaSalami <alirezasalami138010@gmail.com>
2024-05-19 03:24:37 +0330

    local

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['213.130.147.111', 'bhseeds.com']
+ALLOWED_HOSTS = ['213.130.147.111', 'bhseeds.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
