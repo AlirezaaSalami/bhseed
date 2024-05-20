08ecd2040e6b2e0e8bb3a4e4784182a1aad86b9b
AlirezaaSalami <alirezasalami138010@gmail.com>
2024-05-20 22:04:26 +0330

    exo6

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, ProductDetailView
+from .views import HomeView, ProductDetailView, ExoticView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product.detail'),
+    path('exotic/', ExoticView.as_view(), name='exotic'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView , TemplateView
 from .models import Product
 
 
@@ -12,3 +12,7 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
     context_object_name = 'product'
+
+
+class ExoticView(TemplateView):
+    template_name = 'exotic.html'
