f38faf887aef8b5bdc95db13c7ba51cf012e4e7b
AlirezaaSalami <alirezasalami138010@gmail.com>
2024-09-06 13:41:11 +0330

    v2

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,21 +1,16 @@
 const productFilters = document.querySelectorAll('.filters-title')
-const productItems = document.querySelectorAll('.product-item')
+const productItems = document.querySelectorAll('.product-item-box')
 const productFiltersItems = document.querySelectorAll('.product-filters-item')
+const allCompanies = document.querySelector('.companies')
 productFilters.forEach(productFilter => {
     productFilter.addEventListener('click', () => {
         productFilters.forEach(productFilter => {
             productFilter.classList.remove('active')
         })
         productFilter.classList.add('active')
-        if(!productFilter.classList.contains('all-filters')){
+        if (!productFilter.classList.contains('companies')) {
             productFilter.nextElementSibling.classList.toggle('hidden')
         }
-        productItems.forEach(productItem => {
-            if (productFilter.dataset.filter == 'all') {
-                productItem.classList.remove('hidden')
-            }
-        })
-
     })
 })
 
@@ -24,9 +19,21 @@ productFiltersItems.forEach(productFiltersItem => {
         productItems.forEach(productIteme => {
             if (!productIteme.dataset.itemattr.includes(productFiltersItem.dataset.filter)) {
                 productIteme.classList.add('hidden')
-            }else {
+            } else {
                 productIteme.classList.remove('hidden')
             }
         })
     })
 })
+
+function allCompaniesFilter() {
+    productItems.forEach(productIteme => {
+        if (!productIteme.dataset.itemattr.includes(allCompanies.dataset.filter)) {
+            productIteme.classList.add('hidden')
+        } else {
+            productIteme.classList.remove('hidden')
+        }
+    })
+}
+allCompaniesFilter()
+allCompanies.addEventListener('click', allCompaniesFilter)
